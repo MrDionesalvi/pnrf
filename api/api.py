8497639a44f7e5a6efc3569d8bdffa98eb6d7ca3
MrDionesalvi <dionesalvip@gmail.com>
2021-10-10 15:56:51 +0200

    api

--- api/api.py
+++ api/api.py
@@ -13,6 +13,19 @@ api = Blueprint('api', __name__)
 def list():
     return 'helo!'
 
+@api.route('/checkplayer')
+def checkplayer():
+    if 'player' in request.args:
+        player = request.args['player']
+        player = html.escape(player)
+        db = Database()
+        db_session = db.session
+        try:
+            data = db_session.query(db.frequency).filter(db.frequency.username == player).all()
+            if data:
+                return '{["status"]="OK"}'
+        except:
+            return '{["status"]="NP"}'
 
 
 @api.route('/version/lua')
